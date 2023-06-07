19f0b1436a5a87a1ad67172bcabafae8888dcf24
kulsumansari <kulsumansari4@gmail.com>
2023-06-07 19:48:36 +0530

    next/image implementaion reverted

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,18 +1,15 @@
 import { ImageComponent } from '@/types/components'
-import NextImage from 'next/image'
 
 const Image: React.FC<ImageComponent> = (props: ImageComponent) => {
     const { image, image_alt_text, className} = props
    
 
     return <>
-        {image?.url && <NextImage 
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        {image?.url && <img 
             src={image.url}
             alt={image_alt_text || ''}
             className={className || ''}
-            width='0'
-            height='0'
-            sizes='100vw'
         />}
     </>
 }
