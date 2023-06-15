11d5da420e17b7dc1eec666a85d42c632d8f464c
prashanth-contentstack <prashanth.srinivasan@contentstack.com>
2023-06-15 18:03:58 +0530

    card content content overlap fix

--- src/components/CardCollection/CardCollection.tsx
+++ src/components/CardCollection/CardCollection.tsx
@@ -11,36 +11,51 @@ const CardCollection: React.FC<CardCollectionProps> = (props: CardCollectionProp
     const { header, cards } = props
 
     return (
-        
-        <div className={'pb-8 px-8 pb-8 sm:pb-12'}>
+
+        <div className={'py-8 px-8 sm:pb-12'}>
             <div className='max-w-7xl mx-auto '>
-            
+
                 <CardCollectionHeader
                     heading={header?.[0]?.heading}
                     sub_heading={header?.[0]?.sub_heading}
-                    $={{...header?.[0]?.$}}
+                    $={{ ...header?.[0]?.$ }}
                 />
-           
-                <div
-                    className={classNames(
-                        cards?.length === 1 ? 'sm:grid-cols-1 lg:grid-cols-1'
-                            : cards?.length === 2 ? 'sm:grid-cols-2 lg:grid-cols-2'
-                                : cards?.length === 3 ? 'sm:grid-cols-2 lg:grid-cols-3'
-                                    : cards?.length === 4 ? 'sm:grid-cols-2 lg:grid-cols-4'
-                                        : 'sm:grid-cols-2 lg:grid-cols-4',
-                        'grid grid-cols-1 gap-y-12 sm:gap-x-6 xl:gap-x-8'
-                    )}
-                >
-                    {cards?.map((cardData: any, idx: number) => {
-                        //eslint-disable-next-line
-                        return (<Card
-                            key={idx}
-                            {...cardData}
-                            count={cards.length}
-                        />
-                        )
-                    })
-                    }
+
+                    <div
+                        className={
+                            classNames(
+                                'grid relative',
+                                cards?.length === 1
+                                    ?
+                                    'grid-cols-1 lg:grid-cols-1'
+                                    :
+                                    cards?.length === 2
+                                        ?
+                                        'grid-cols-2 lg:grid-cols-2'
+                                        :
+                                        cards?.length === 3
+                                            ?
+                                            'grid-cols-2 lg:grid-cols-3'
+                                            :
+                                            cards?.length === 4
+                                                ?
+                                                'sm:grid-cols-2 lg:grid-cols-4'
+                                                :
+                                                'grid-cols-2 lg:grid-cols-4',
+                                'gap-x-6 xl:gap-x-8 gap-y-12'
+                            )
+                        }
+                    >
+                        {cards?.map((cardData: any, idx: number) => {
+                            //eslint-disable-next-line
+                            return (<Card
+                                key={idx}
+                                {...cardData}
+                                count={cards.length}
+                            />
+                            )
+                        })
+                        }
                 </div>
             </div>
         </div>
--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -4,7 +4,7 @@ import { ImageCardItem } from '@/types/components'
 import { classNames } from '@/utils'
 
 const Card: React.FC<ImageCardItem> = (props: ImageCardItem) => {
-    const { $, image, image_alt_text, title, cta, content,count } = props
+    const { $, image, image_alt_text, title, cta, content, } = props
 
     return (
         <div
@@ -18,12 +18,12 @@ const Card: React.FC<ImageCardItem> = (props: ImageCardItem) => {
                     image_alt_text={image_alt_text}
                     // className={'w-full object-center object-fit object-cover h-48 xl:h-40'}
                     className={classNames(
-                        count === 1 ? '!h-full'
-                            :count === 2 ? 'lg:h-64' 
-                                :count === 3 ? 'lg:h-52'
-                                    : count && count >= 4 ? 'lg:h-40'
-                                        : '',
-                        'h-48 w-full object-center object-fit object-cover'
+                        // count === 1 ? '!h-full'
+                        //     :count === 2 ? 'lg:h-64' 
+                        //         :count === 3 ? 'lg:h-52'
+                        //             : count && count >= 4 ? 'lg:h-40'
+                        //                 : '',
+                        'w-full object-center object-fit object-cover'
                     )}
                 />
                 }
@@ -39,16 +39,19 @@ const Card: React.FC<ImageCardItem> = (props: ImageCardItem) => {
                     {content}
                 </p>
                 }
+
+                {cta?.link?.length !== 0 && cta?.text && <p className='mt-3 text-base font-semibold !text-purple'>
+                    <Link
+                        url={cta?.link}
+                        className='!text-purple hover:border-b-2 hover:border-purple cursor-pointer'
+                    >
+                        {cta.text}
+                    </Link> &rarr;
+                </p>
+                }
             </div>
 
-            {cta?.link?.length !== 0 && cta?.text && <span className='mt-3 text-base font-semibold !text-purple'>
-                <Link
-                    url={cta?.link}
-                    className='!text-purple hover:border-b-2 hover:border-purple cursor-pointer'
-                >
-                    {cta.text}
-                </Link> &rarr;
-            </span>}
+
         </div>
     )
 }
