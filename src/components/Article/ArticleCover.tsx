deee6fb7eae7d264f3c2c9b47e7ba75710bb66f8
mudassar-web <mudassar.ansari@gmail.com>
2023-06-16 12:13:21 +0530

    TSOND-113 | Live preview rectified on Article

--- src/components/Article/ArticleCover.tsx
+++ src/components/Article/ArticleCover.tsx
@@ -7,9 +7,8 @@ const ArticleCover:React.FC<ArticleCover> = (props:ArticleCover) => {
     return( <div className={'px-8 pt-16'}>
         <div
             className='container mx-auto'
-            {...$?.title}
         >
-            {title && <h2 className='text-3xl font-bold tracking-tight sm:text-4xl'>{title}</h2>}
+            {title && <h2 className='text-3xl font-bold tracking-tight sm:text-4xl' {...$?.title}>{title}</h2>}
             <div className={'relative overflow-hidden mt-6'}>
                 {/* eslint-disable-next-line jsx-a11y/alt-text */}
                 {cover_image?.url && <Image
@@ -18,7 +17,7 @@ const ArticleCover:React.FC<ArticleCover> = (props:ArticleCover) => {
                     className={'w-full max-auto object-cover aspect-[2] md:aspect-[3/1] object-center bg-white/5 shadow-2xl ring-1 ring-white/10 dark:text-white'}
                 />}
             </div>
-            {summary && <p className='mt-6 text-lg leading-8 text-gray-600 dark:text-white'>
+            {summary && <p className='mt-6 text-lg leading-8 text-gray-600 dark:text-white' {...$?.summary}>
                 {summary}
             </p>}
         </div>
