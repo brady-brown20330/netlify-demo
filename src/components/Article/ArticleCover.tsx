6718b0e45d19231230b61c0add1167d565f09691
kulsumansari <kulsumansari4@gmail.com>
2023-06-23 13:09:41 +0530

    changed cover-image aspect ratio

--- src/components/Article/ArticleCover.tsx
+++ src/components/Article/ArticleCover.tsx
@@ -13,7 +13,7 @@ const ArticleCover:React.FC<ArticleCover> = (props:ArticleCover) => {
                 {/* eslint-disable-next-line jsx-a11y/alt-text */}
                 {cover_image?.url && <Image
                     image={cover_image}
-                    className={'w-full max-auto object-cover aspect-[2] md:aspect-[3/1] object-center bg-white/5 shadow-2xl ring-1 ring-white/10 dark:text-white'}
+                    className={'w-full max-auto object-cover aspect-[2] md:aspect-[5/2] object-center bg-white/5 shadow-2xl ring-1 ring-white/10 dark:text-white'}
                 />}
             </div>
             {summary && <p className='mt-6 text-lg leading-8 font-semibold text-gray-700 dark:text-white' {...$?.summary}>
